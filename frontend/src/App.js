fd06900be243b0043caa51213ee9015625e67513
Kanav Arora <kanavarora1515@gmail.com>
2023-09-17 23:14:05 +0530

    Update frontend/src/App.js
    Co-authored-by: AADI JAIN <jainaadi7102@gmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-import Home from './components/Home/';
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Team from './components/Meet Team'
 import {
   Routes, Route
 } from "react-router-dom";
+
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Home from './components/Home/';
+import Team from './components/Meet Team'
 function App() {
   return (
     <>
