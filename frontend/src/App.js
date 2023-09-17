c3a6a1f6b8fc2ee7b509aa7fd345395fb5cee0eb
Kanav Arora <kanavarora1515@gmail.com>
2023-09-17 23:42:37 +0530

    PR Approved requested changes done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,8 @@
 import './App.css';
+import Home from './components/Home/';
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Team from './components/Team'
 import {
   Routes, Route
 } from "react-router-dom";
--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -1,3 +1,3 @@
-import React from 'react';
 import Home from './Landing'
+
 export default Home;
