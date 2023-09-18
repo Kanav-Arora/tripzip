167143fe2de18ae439082153a0b6b4dffe281270
Kanav Arora <kanavarora1515@gmail.com>
2023-09-18 15:21:00 +0530

    Bug fixes in Frontend/App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,10 +7,6 @@ import {
   Routes, Route
 } from "react-router-dom";
 
-import Footer from './components/Footer'
-import Header from './components/Header'
-import Home from './components/Home/';
-import Team from './components/Meet Team'
 function App() {
   return (
     <>
