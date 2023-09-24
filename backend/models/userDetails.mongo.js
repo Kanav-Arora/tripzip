91b59ea78b1a6c3bacb83945bd4aebd22ffbc765
Kanav Arora <kanavarora1515@gmail.com>
2023-09-24 16:59:05 +0530

    User Details Schema Added

---
+++ backend/models/userDetails.mongo.js
@@ -0,0 +1,52 @@
+// Creating User Detail Schema
+const mongoose = require('mongoose')
+const { Schema } = mongoose;
+
+const userDetailSchema = new Schema({
+    uid: {
+        type: Schema.Types.ObjectId,
+        required: [true, 'No UID found'],
+        unique: true
+    },
+    address: {
+        type: Schema.Types.String,
+    },
+    pincode: {
+        type: Schema.Types.Number,
+    },
+    city: {
+        type: Schema.Types.String,
+    },
+    state: {
+        type: Schema.Types.String,
+    },
+    country: {
+        type: Schema.Types.String,
+    },
+    age: {
+        type: Number,
+    },
+    gender: {
+        type: Schema.Types.String,
+    },
+    tripsCreated: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Trips'
+    }],
+    tripsInterested: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Trips'
+    }],
+    status: {
+        type: Schema.Types.String,
+        enum: ['active', 'inactive', 'deleted'],
+        default: 'active'
+    },
+    updatedAt: {
+        type: Schema.Types.Date,
+        default: Date.now
+    }
+});
+
+const UserDetails = mongoose.model('UserDetails', userDetailSchema);
+module.exports = UserDetails;
