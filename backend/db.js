c8e9e06a00883ce87fea7df13338f89622db9d7e
Kanav Arora <kanavarora1515@gmail.com>
2023-10-16 22:45:53 +0530

    fix(db): prevent re-establish of connection

--- backend/db.js
+++ backend/db.js
@@ -4,8 +4,18 @@ const logger = require('./utils/logger/logger');
 
 const mongoURI = config.MongodbUri;
 
+let db;
+
 async function connectToMongo() {
-  await mongoose.connect(mongoURI).then(() => logger.info('Connected to Mongo Successfully')).catch((err) => logger.error(err));
+  if (!db) {
+    try {
+      db = await mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+      logger.info('Connected to Mongo Successfully');
+    } catch (error) {
+      logger.error(error);
+    }
+  }
+  return db;
 }
 
 module.exports = connectToMongo;
