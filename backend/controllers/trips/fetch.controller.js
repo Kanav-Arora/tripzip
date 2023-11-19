c19b8f3fce476cba3c87a59f4bbeb8ae3759d578
Kanav Arora <kanavarora1515@gmail.com>
2023-11-19 15:27:07 +0530

    39-feat(backend): pagination and sorting of results added (#40)
    * 39-feat(backend): pagination and sorting of results added
    
    * 39-feat(frontend): bug fixes
    
    * Top Loading Bar (#45)
    
    * 44-feat(front): top loading bar added
    
    * fix(front): date picker positioning
    
    * feat(infra): env config added for login
    
    * fix(backend): import fixes

--- backend/controllers/trips/fetch.controller.js
+++ backend/controllers/trips/fetch.controller.js
@@ -1,7 +1,7 @@
-const logger = require('../../utils/logger/logger');
 const Trips = require('../../models/trip.mongo');
+const logger = require('../../utils/logger/logger');
 
-async function filteredTrips(req, res) {
+async function countTrips(req, res) {
     try {
         const {
             startDate, endDate, destination, uid,
@@ -23,11 +23,59 @@ async function filteredTrips(req, res) {
             filter.tripDetails.endDate = { $lte: new Date(endDate) };
         }
 
-        const trips = await Trips.find(filter).populate('tripDetails');
+        const tripsCount = await Trips.count(filter);
+
+        res.status(200).json({
+            status: 200,
+            message: 'Trips Results Fetched successfully',
+            data: { tripsCount },
+        });
+    } catch (error) {
+        logger.error(error);
+        res.status(500).send({
+            status: 500,
+            message: 'Internal Server Error',
+        });
+    }
+}
+
+async function filteredTrips(req, res) {
+    try {
+        const {
+            startDate, endDate, destination, uid,
+        } = req.query;
+
+        const page = req.body.page || 1;
+        const limit = req.body.limit || 20;
+
+        const startIndex = (page - 1) * limit;
+
+        const filter = {
+            status: 'active',
+        };
+        if (uid) {
+            filter.createdBy = uid;
+        }
+        if (destination) {
+            filter.tripDetails.city = destination.replace(/-/g, ' ');
+        }
+        if (startDate) {
+            filter.tripDetails.startDate = { $gte: new Date(startDate) };
+        }
+        if (endDate) {
+            filter.tripDetails.endDate = { $lte: new Date(endDate) };
+        }
+
+        const trips = await Trips.find(filter)
+            .populate('tripDetails')
+            .sort({ views: -1 })
+            .skip(startIndex)
+            .limit(limit);
+
         res.status(200).json({
             status: 200,
             message: 'Trips fetched successfully',
-            data: trips,
+            data: { trips },
         });
     } catch (error) {
         logger.error(error);
@@ -60,4 +108,4 @@ async function fetchTripByID(req, res) {
     }
 }
 
-module.exports = { filteredTrips, fetchTripByID };
+module.exports = { countTrips, filteredTrips, fetchTripByID };
